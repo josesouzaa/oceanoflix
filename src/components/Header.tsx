63a19f1372c1556b72f00db20fd8c4b5505d5361
josesouzaa <josee.souzaaa@gmail.com>
2022-07-22 14:00:54 -0300

    Menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,8 +19,25 @@ export function Header() {
           <span className="text-brand-green-400">Oceano</span>Flix
         </h1>
 
-        <nav>
+        <nav className="hidden sm:block">
           <ul className="flex items-center gap-4">
+            {itensMenu.map((i) => (
+              <li
+                key={i.slug}
+                className="hover:text-brand-green-400 font-semibold transition"
+              >
+                <Link href={i.slug}>{i.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <nav className="block sm:hidden relative group">
+          <span className="font-bold bg-brand-green-400 text-brand-blue-900 p-1 rounded-sm cursor-pointer group-hover:bg-brand-blue-900 group-hover:text-brand-green-400 transition">
+            Menu
+          </span>
+
+          <ul className="flex flex-col items-center px-4 py-2 rounded-sm bg-brand-blue-900 gap-2 absolute top-[100%] right-0 invisible max-h-0 group-hover:visible group-hover:max-h-fit hover:visible hover:max-h-full border-t-4 border-brand-blue-400 transition">
             {itensMenu.map((i) => (
               <li
                 key={i.slug}
